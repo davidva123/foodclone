3f71fc9d59efc120833417db6bca17790d4c521f
david <13765965+davidva123@users.noreply.github.com>
2020-05-18 17:21:21 -0400

    base code done

---
+++ src/FoodClone.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation";
+
+class FoodClone extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="container-fluid">
+        <Navigation />
+        
+      </div>
+    );
+  }
+}
+
+export default FoodClone;
---
+++ src/components/JumboTron.jsx
@@ -0,0 +1,8 @@
+class JumboTron extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default JumboTron;
