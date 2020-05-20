72fd944b50548ac90c9691847c3e483b20aea7b8
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-05-20 22:50:02 +0100

    Adding homepages and routes files

---
+++ src/pages/RecipeDetail/RecipeDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Recipe = () => {
+  return (
+    <div>
+      <h1>Recipe - Burger and Chips</h1>
+    </div>
+  );
+};
---
+++ src/pages/RecipeHome/RecipeHome.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const RecipeHome = () => {
+  return (
+    <div>
+      <h1>Recipes</h1>
+    </div>
+  );
+};
