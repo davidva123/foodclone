1cf88f31953632b44bdfd23529efd51f8da43472
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-05-21 10:02:13 +0100

    removing unneeded text used for texting

--- src/pages/RecipeDetail/RecipeDetail.jsx
+++ src/pages/RecipeDetail/RecipeDetail.jsx
@@ -5,12 +5,6 @@ const RecipeDetail = (props) => {
   return (
     <div>
       <h1>Recipe Detail for recipe id: {recipeId}</h1>
-      <p>
-        Elit in non do aliquip ex quis tempor commodo amet aliquip ipsum non
-        proident qui. Laboris esse elit reprehenderit quis eiusmod mollit cillum
-        minim sit ipsum non. Ex ea mollit velit voluptate eu veniam sint ea in
-        labore aliquip dolore.
-      </p>
     </div>
   );
 };
--- src/pages/RecipeHome/RecipeHome.jsx
+++ src/pages/RecipeHome/RecipeHome.jsx
@@ -4,14 +4,6 @@ const RecipeHome = () => {
   return (
     <div>
       <h1>Recipes</h1>
-      <p>
-        Eu ullamco ipsum proident cillum excepteur nisi Lorem qui deserunt ipsum
-        minim aliquip qui. Exercitation in dolor sunt adipisicing veniam commodo
-        ut mollit laborum consectetur minim esse. Enim quis proident Lorem qui
-        nulla occaecat nisi sit. Magna elit esse reprehenderit in consectetur
-        nulla voluptate. Reprehenderit pariatur ut reprehenderit Lorem deserunt
-        dolor minim fugiat voluptate. Laborum qui amet commodo labore et.
-      </p>
     </div>
   );
 };
