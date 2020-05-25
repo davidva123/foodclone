b5a2fca2b7d1b2ed38d5e636416b4e238dc20558
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-05-25 19:41:32 +0100

    comments

--- src/contexts/RecipeContext.js
+++ src/contexts/RecipeContext.js
@@ -3,6 +3,7 @@ import React, { createContext, useContext } from "react";
 const intialRecipes = { value: "loaded" };
 export const RecipeContext = createContext(intialRecipes);
 
+//Provider Component is wrapped round the App in Index
 const RecipeProvider = ({ children }) => {
   return (
     <RecipeContext.Provider value={intialRecipes}>
@@ -13,6 +14,7 @@ const RecipeProvider = ({ children }) => {
 
 export default RecipeProvider;
 
+// useRecipes hook to be used withing a components to access recipes context
 const useRecipes = () => {
   const recipes = useContext(RecipeContext);
   return recipes;
