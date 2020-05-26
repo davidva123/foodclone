90cc45f8ffb5b9e93da9c718d42d329c02f953c1
david <13765965+davidva123@users.noreply.github.com>
2020-05-26 09:14:54 -0400

    added recipelistselector

--- src/components/RecipeListFilter/RecipeListFilter.jsx
+++ src/components/RecipeListFilter/RecipeListFilter.jsx
@@ -1,12 +1,15 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState } from "react";
 
 const RecipeListFilter = () => {
-    const [recipeList,setRecipeList] = useState('General');
-    return ( 
-        <select>
-            <option>{recipeList}</option>
-        </select>
-     );
-}
- 
+  
+  const recipeListItems = ["A", "B", "C", "D"];
+  return (
+    <select>
+      {recipeListItems.map((listItem) => (
+        <option value={listItem}>{listItem}</option>
+      ))}
+    </select>
+  );
+};
+
 export default RecipeListFilter;
