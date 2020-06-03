de88bc4b54afc056876a92cc46d0235c91871e18
david <13765965+davidva123@users.noreply.github.com>
2020-06-03 15:46:26 -0400

    getting dummy data to render on the page

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -3,10 +3,13 @@ import StarRating from "../StarRating/StarRating";
 import CookTime from "../CookTime/CookTime";
 import "./RecipeCard.scss";
 
-export const RecipeCard = () => {
+export const RecipeCard = (props) => {
+console.log('Recipe >>',props.recipe);
+const {name,n_step, minutes} = props.recipe;
+
   return (
     <div className="RecipeCard col-md-4">
-      <h1>Title Goes here1</h1>
+      <h1>{name}</h1>
       <span>By: Variable</span>
       <p>Blurb goes here</p>
       <div className="row">
@@ -15,7 +18,7 @@ export const RecipeCard = () => {
         </span>
         <span className="col-lg-2">
           <p>
-            Time: <CookTime cookTime={12} />
+            Time: <CookTime cookTime={minutes} />
           </p>
         </span>
       </div>
