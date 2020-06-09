ada76f59402e2dd6d8728c33ff98882127c3d701
david <13765965+davidva123@users.noreply.github.com>
2020-06-09 15:48:40 -0400

    Doing fixes so the code works

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -3,12 +3,14 @@ import StarRating from "../StarRating/StarRating";
 import CookTime from "../CookTime/CookTime";
 import "./RecipeCard.scss";
 
-export const RecipeCard = () => {
+export const RecipeCard = (props) => {
+  const { name, n_step, minutes, contributor_name, description } = props.recipe;
+
   return (
     <div className="RecipeCard col-md-4">
-      <h1>Title Goes here1</h1>
-      <span>By: Variable</span>
-      <p>Blurb goes here</p>
+      <h1>{name}</h1>
+      <span>By: {contributor_name}</span>
+      <p>{description.length > 50 ? description.slice(1,100) + '...' : description}</p>
       <div className="row">
         <div className="col-lg-2">
           <StarRating rating={3} />
--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import RecipeListFilter from "../RecipeListFilter/RecipeListFilter";
 import { RecipeCard } from "../RecipeCard/RecipeCard";
-
+import { RecipeContext } from "../../contexts/RecipeContext";
 export const RecipeList = () => {
+  const recipes = useContext(RecipeContext);
   return (
     <div className=" recipe-list row justify-content-md-center ">
       <div className="col col-lg-5">
         {/* <RecipeListFilter />  removed due to nonuse */}
-        {/* {recipes.map(recipe => <RecipeCard recipe={recipe}/>)} */}
+       
         <div className=" row  ">
-          <RecipeCard starRating={1} />
+          {/* <RecipeCard starRating={1} />
           <RecipeCard starRating={2} />
           <RecipeCard starRating={3} />
           <RecipeCard starRating={4.5} />
+
+           */}
+        {recipes.map(recipe => <RecipeCard recipe={recipe}/>)}
         </div>
       </div>
     </div>
