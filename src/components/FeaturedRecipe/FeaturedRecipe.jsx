d04bd9a7e2b5c21c3134f40541a48b5b16e9a76f
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-06-11 20:58:27 +0100

    Componenet Setup

---
+++ src/components/FeaturedRecipe/FeaturedRecipe.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FeaturedRecipe.scss";
+
+export const FeaturedRecipe = () => {
+  return (
+    <div className="FeaturedRecipe">
+      <h3>Featured Recipe</h3>
+    </div>
+  );
+};
