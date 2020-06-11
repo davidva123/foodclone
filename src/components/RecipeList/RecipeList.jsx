bc830ce1dd3a3772139986ad78cccd771c17e117
david <13765965+davidva123@users.noreply.github.com>
2020-06-11 17:10:59 -0400

    working on code for recipecard

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -5,7 +5,7 @@ export const RecipeList = () => {
   const recipes = useContext(RecipeContext);
   return (
     <div className=" recipe-list row justify-content-md-center ">
-      <div className="col col-lg-5">
+      <div className="col col-12 col-md-6 col-lg-9">
         <div className=" row  ">
           {recipes.map((recipe, i) => (
             <RecipeCard key={`RecipeCard-${i}`} recipe={recipe} />
