04c752f18b6d79f501507e28d8f89332e8d2e131
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-06-11 20:49:55 +0100

    misspelled import Navbar

--- src/FoodClone.js
+++ src/FoodClone.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import RecipeList from "./components/RecipeList/RecipeList";
-import Navigation from "./components/navbar/NavBar";
+import Navigation from "./components/Navbar/NavBar";
 
 function FoodClone() {
   return (
